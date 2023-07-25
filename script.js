7580ab42cfeefb61d803a5e6e9da2fdb7f8044b9
iqbalhossain4 <iqbal-hossain@mail.com>
2023-07-25 16:16:43 +0600

    solve first one

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //Task 1 : Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+function stringRevers(str) {
+  let strContainer = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    strContainer += str[i];
+  }
+  return strContainer;
+}
+
+console.log(stringRevers("hello world"));
