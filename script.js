a265ae8cc32c6f28b01c3abd8983d1179e679832
iqbalhossain4 <iqbal-hossain@mail.com>
2023-07-25 20:52:05 +0600

    fix some isues

--- script.js
+++ script.js
@@ -110,3 +110,5 @@ function generateRandomPass(passLength) {
 console.log(generateRandomPass(12));
 
 //Task 8: Implement a JavaScript function to find the second smallest element in an array of numbers. The function should return the second smallest number.
+
+// fff
